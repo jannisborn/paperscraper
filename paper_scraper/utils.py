c8d362710cea8292bc0679654d74414d626e03cd
Jannis Born <jab@zurich.ibm.com>
2020-07-01 23:27:00 +0200

    feat: utils for dumping jsonl

---
+++ paper_scraper/utils.py
@@ -0,0 +1,16 @@
+from typing import List
+
+
+def dump_papers(papers: List[dict], filepath: str) -> None:
+    """
+    Receives a list of dicts, one dict per paper and dumps it into a .jsonl
+    file with one paper per line.
+
+    Args:
+        - papers (list[dict]): List of papers
+        - filepath (str): Path to dump the papers.
+    """
+
+    with open(filepath, 'w') as f:
+        for paper in papers:
+            f.write(str(paper), '\n')
