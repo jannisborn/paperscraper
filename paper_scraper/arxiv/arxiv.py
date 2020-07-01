f5761232de4f5a6388ed29327845de9523bc93c9
Jannis Born <jab@zurich.ibm.com>
2020-07-01 23:26:35 +0200

    feat: Utils for arxiv querying

---
+++ paper_scraper/arxiv/arxiv.py
@@ -0,0 +1,66 @@
+from typing import List
+import arxiv
+from paper_scraper.utils import dump_papers
+from paper_scraper.arxiv.utils import get_query_from_keywords
+
+arxiv_field_mapper = {
+    'published': 'date',
+    'journal_reference': 'journal',
+    'summary': 'abstract'
+}
+
+
+def get_arxiv_papers(
+    query: str,
+    fields: List = ['title', 'authors', 'date', 'abstract', 'journal', 'doi'],
+    *args,
+    **kwargs
+):
+    """
+    Performs arxiv API request of a given query and returns list of papers with
+    fields as desired.
+
+    Args:
+        query (str): Query to arxiv API. Needs to match the arxiv API notation.
+        fields (list[str]): List of strings with fields to keep in output.
+        *args, **kwargs are additional arguments for arxiv.query
+
+    Returns:
+        list of dicts. One dict per paper.
+
+    """
+    raw = arxiv.query(query=query, *args, **kwargs)
+
+    processed = [
+        {
+            arxiv_field_mapper.get(key, key): value
+            for key, value in paper.items()
+            if arxiv_field_mapper.get(key, key) in fields
+        } for paper in raw
+    ]
+    return processed
+
+
+def get_and_dump_arxiv_papers(
+    keywords: List[str, List[str]],
+    filepath: str,
+    fields: List = ['title', 'authors', 'date', 'abstract', 'journal', 'doi'],
+    *args,
+    **kwargs
+):
+    """
+    Combines get_arxiv_papers and dump_papers.
+
+    Args:
+        keywords (List[str, List[str]]): List of keywords to request arxiv API.
+            The outer list level will be considered as AND separated keys, the
+            inner level as OR separated.
+        filepath (str): Path where the dump will be saved.
+        fields (List, optional): List of strings with fields to keep in output.
+            Defaults to ['title', 'authors', 'date', 'abstract',
+            'journal', 'doi'].
+    """
+    # Translate keywords into query.
+    query = get_query_from_keywords(keywords)
+    papers = get_arxiv_papers(query, fields, *args, **kwargs)
+    dump_papers(papers, filepath)
---
+++ paper_scraper/arxiv/utils.py
@@ -0,0 +1,26 @@
+from typing import List
+
+finalize_disjunction = lambda x: '(' + x[:-4] + ') AND '
+finalize_conjunction = lambda x: x[:-5]
+
+
+def get_query_from_keywords(keywords: List[str, List[str]]) -> str:
+    """Receives a list of keywords and returns the query for the arxiv API.
+
+    Args:
+        keywords (List[str, List[str]]): Items will be AND separated. If items
+            are lists themselves, they will be OR separated.
+
+    Returns:
+        str: query to enter to arxiv API.
+    """
+
+    query = ''
+    for i, key in enumerate(keywords):
+        if isinstance(key, str):
+            query += f'all:{key} AND '
+        elif isinstance(key, list):
+            inter = ''.join([f'all:{syn} OR ' for syn in key])
+            query += finalize_disjunction(inter)
+
+    query = finalize_conjunction(query)
