cb748d91459f2c20ad5070b3183f63b4bfcb2b2e
Jannis Born <jab@zurich.ibm.com>
2020-11-11 10:33:43 +0100

    doc: Set logger levels

--- paperscraper/__init__.py
+++ paperscraper/__init__.py
@@ -13,12 +13,22 @@ from .utils import get_filename_from_query
 logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
 logger = logging.getLogger(__name__)
 
+# Set urllib logging depth
+url_logger = logging.getLogger('urllib3')
+url_logger.setLevel(logging.WARNING)
+# Set arxiv logging depth
+arxiv_logger = logging.getLogger('arxiv')
+arxiv_logger.setLevel(logging.WARNING)
+
 
 def dump_queries(keywords: List[List[Union[str, List[str]]]], dump_root: str) -> None:
     """Performs keyword search on all available servers and dump the results.
 
     Args:
-        keywords (list): List of lists of keywords.
+        keywords (List[List[Union[str, List[str]]]]): List of lists of keywords. Each
+            second-level list is considered a separate query. Within each query, each
+            item (whether str or List[str]) are considered AND separated. If an item
+            is again a list, strs are considered synonyms (OR separated).
         dump_root (str): Path to root for dumping.
     """
 
@@ -27,4 +37,5 @@ def dump_queries(keywords: List[List[Union[str, List[str]]]], dump_root: str) ->
 
             logger.info(f' Keyword {idx+1}/{len(keywords)}, DB: {db}')
             filename = get_filename_from_query(keyword)
+            os.makedirs(os.path.join(dump_root, db), exist_ok=True)
             f(keyword, output_filepath=os.path.join(dump_root, db, filename))
