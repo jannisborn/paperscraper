1b8873923ced59fbfc60eb7ca5a39d58f14e80e6
jannisborn <jannis.born@gmx.de>
2021-10-17 23:20:05 +0200

    feat: Bring back ChemRxiv API

--- paperscraper/get_dumps/chemrxiv.py
+++ paperscraper/get_dumps/chemrxiv.py
@@ -11,12 +11,12 @@ from .utils.chemrxiv import ChemrxivAPI, download_full, parse_dump
 logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
 logger = logging.getLogger(__name__)
 
-today = datetime.today().strftime('%Y-%m-%d')
-save_folder = pkg_resources.resource_filename('paperscraper', 'server_dumps')
-save_path = os.path.join(save_folder, f'chemrxiv_{today}.jsonl')
+today = datetime.today().strftime("%Y-%m-%d")
+save_folder = pkg_resources.resource_filename("paperscraper", "server_dumps")
+save_path = os.path.join(save_folder, f"chemrxiv_{today}.jsonl")
 
 
-def chemrxiv(save_path: str = save_path, token=None) -> None:
+def chemrxiv(save_path: str = save_path) -> None:
     """Fetches all papers from biorxiv until current date, stores them in jsonl
     format in save_path.
 
@@ -25,18 +25,9 @@ def chemrxiv(save_path: str = save_path, token=None) -> None:
             Defaults to save_path.
     """
 
-    # API remains down for the moment
-    chemrxiv_unavailable()
-
     # create API client
-    api = ChemrxivAPI(token=token)
+    api = ChemrxivAPI()
     # Download the data
     download_full(save_folder, api)
     # Convert to JSONL format.
     parse_dump(save_folder, save_path)
-
-
-def chemrxiv_unavailable():
-    raise ConnectionRefusedError(
-        'From Sept. 2021 onwards, ChemRxiv API is no accessible (details in README).'
-    )
