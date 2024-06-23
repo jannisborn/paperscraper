f2597bccc5dec063918adf3a652b74bceaa9df00
jannisborn <jannis.born@gmx.de>
2024-06-23 11:44:10 +0200

    test: Fix decorate to handle API outage

--- paperscraper/scholar/tests/test_scholar.py
+++ paperscraper/scholar/tests/test_scholar.py
@@ -2,7 +2,7 @@ import logging
 import pandas as pd
 import pytest
 from scholarly._proxy_generator import MaxTriesExceededException
-
+import functools
 
 from paperscraper.scholar import (
     get_and_dump_scholar_papers,
@@ -13,29 +13,34 @@ from paperscraper.scholar import (
 logging.disable(logging.INFO)
 
 
-@pytest.fixture(autouse=True)
-def handle_scholar_exceptions(caplog):
-    caplog.set_level(logging.INFO)
-    try:
-        yield
-    except MaxTriesExceededException as e:
-        logging.info(f"MaxTriesExceededException caught: {e}")
-        pytest.skip("Skipping test due to MaxTriesExceededException")
+def handle_scholar_exception(func):
+    @functools.wraps(func)
+    def wrapper(*args, **kwargs):
+        try:
+            return func(*args, **kwargs)
+        except MaxTriesExceededException as e:
+            logging.info(f"MaxTriesExceededException caught: {e}")
+            pytest.skip("Skipping test due to MaxTriesExceededException")
+
+    return wrapper
 
 
 class TestScholar:
 
+    @handle_scholar_exception
     def test_citations(self):
         num = get_citations_from_title("GT4SD")
         assert isinstance(num, int)
         assert num > 0
 
+    @handle_scholar_exception
     def test_dump_search(self, tmpdir):
         temp_dir = tmpdir.mkdir("scholar_papers")
         output_filepath = temp_dir.join("results.jsonl")
         get_and_dump_scholar_papers("GT4SD", str(output_filepath))
         assert output_filepath.check(file=1)
 
+    @handle_scholar_exception
     def test_basic_search(self):
         results = get_scholar_papers("GT4SD")
         assert len(results) > 0  # Ensure we get some results
