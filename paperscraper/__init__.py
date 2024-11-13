0afbada80b332cfceb929b01ea11bf5b5aee173d
Jannis Born <jab@zurich.ibm.com>
2024-11-13 09:07:39 +0100

    save_pdf can now also save paper metadata in json format (#57)
    * chore: version and deps bump
    
    * feat: Save json with paper metadata
    
    * test: remove mock of PDF

--- paperscraper/__init__.py
+++ paperscraper/__init__.py
@@ -1,7 +1,7 @@
 """Initialize the module."""
 
 __name__ = "paperscraper"
-__version__ = "0.2.14"
+__version__ = "0.2.15"
 
 import logging
 import os
--- paperscraper/tests/test_pdf.py
+++ paperscraper/tests/test_pdf.py
@@ -5,7 +5,6 @@ from pathlib import Path
 from unittest.mock import MagicMock, mock_open, patch
 
 import pytest
-
 from paperscraper.load_dumps import QUERY_FN_DICT
 from paperscraper.pdf import save_pdf, save_pdf_from_dump
 
@@ -24,9 +23,56 @@ class TestPDF:
 
     def test_basic_search(self):
         paper_data = {"doi": "10.48550/arXiv.2207.03928"}
-        save_pdf(paper_data, filepath="gt4sd_paper.pdf")
-        assert os.path.exists("gt4sd_paper.pdf")
-        os.remove("gt4sd_paper.pdf")
+        save_pdf(paper_data, filepath="gt4sd.pdf", save_metadata=True)
+        assert os.path.exists("gt4sd.pdf")
+        assert os.path.exists("gt4sd.json")
+        os.remove("gt4sd.pdf")
+        os.remove("gt4sd.json")
+
+        # # chemrxiv
+        paper_data = {"doi": "10.26434/chemrxiv-2021-np7xj-v4"}
+        save_pdf(paper_data, filepath="kinases.pdf", save_metadata=True)
+        assert os.path.exists("kinases.pdf")
+        assert os.path.exists("kinases.json")
+        os.remove("kinases.pdf")
+        os.remove("kinases.json")
+
+        # biorxiv
+        paper_data = {"doi": "10.1101/798496"}
+        save_pdf(paper_data, filepath="taskload.pdf", save_metadata=True)
+        assert os.path.exists("taskload.pdf")
+        assert os.path.exists("taskload.json")
+        os.remove("taskload.pdf")
+        os.remove("taskload.json")
+
+        # # medrxiv
+        paper_data = {"doi": "10.1101/2020.09.02.20187096"}
+        save_pdf(paper_data, filepath="covid_review.pdf", save_metadata=True)
+        assert os.path.exists("covid_review.pdf")
+        assert os.path.exists("covid_review.json")
+        os.remove("covid_review.pdf")
+        os.remove("covid_review.json")
+
+        # # journal with OA paper
+        paper_data = {"doi": "10.1038/s42256-023-00639-z"}
+        save_pdf(paper_data, filepath="regression_transformer", save_metadata=True)
+        assert os.path.exists("regression_transformer.pdf")
+        assert os.path.exists("regression_transformer.json")
+        os.remove("regression_transformer.pdf")
+        os.remove("regression_transformer.json")
+
+        # book chapter with paywall
+        paper_data = {"doi": "10.1007/978-981-97-4828-0_7"}
+        save_pdf(paper_data, filepath="clm_chapter", save_metadata=True)
+        assert not os.path.exists("clm_chapter.pdf")
+        assert os.path.exists("clm_chapter.json")
+        os.remove("clm_chapter.json")
+
+        # journal without OA paper
+        paper_data = {"doi": "10.1126/science.adk9587"}
+        save_pdf(paper_data, filepath="color", save_metadata=True)
+        assert not os.path.exists("color.pdf")
+        assert not os.path.exists("color.json")
 
     def test_missing_doi(self):
         with pytest.raises(KeyError):
@@ -80,20 +126,6 @@ class TestPDF:
         mock_get.side_effect = [response_doi, Exception("Network error")]
         save_pdf(paper_metadata=paper_data, filepath="output.pdf")
         assert not os.path.exists("output.pdf")
-
-    @patch("requests.get")
-    @patch("builtins.open", new_callable=mock_open)
-    def test_successful_pdf_download_and_save(self, mock_file, mock_get, paper_data):
-        response_doi = MagicMock()
-        response_doi.text = (
-            '<meta name="citation_pdf_url" content="http://valid.url/document.pdf">'
-        )
-        response_pdf = MagicMock()
-        response_pdf.content = b"PDF content"
-        mock_get.side_effect = [response_doi, response_pdf]
-        save_pdf(paper_metadata=paper_data, filepath="output.pdf")
-        mock_file.assert_called_once_with("output.pdf", "wb+")
-        mock_file().write.assert_called_once_with(b"PDF content")
 
     def test_save_pdf_from_dump_without_path(self):
         with pytest.raises(ValueError):
